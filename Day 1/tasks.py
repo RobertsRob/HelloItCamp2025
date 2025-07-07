27f503506077891ed2385f18f4392e744bff89ff
RobertsTr <troksaroberts@gmail.com>
2025-07-07 13:23:47 +0300

    Addet day 1 and day 2 presentations about python, also added day 1 tasks

---
+++ Day 1/tasks.py
@@ -0,0 +1,26 @@
+# 1. Uzrakstiet programmu, kas izdrukā divas rindiņas atsevišķās rindās: "Labrīt!" un "Kā tev iet?"
+
+# 2. Izvadiet vārdu "Hello" tā, lai konsolē katrs burts būtu viens zem otra!
+
+# 3. Uzrakstiet programmu, kas izdrukā divu skaitļu pievienošanas rezultātu: 78545 un 398.
+print(78545 + 398)
+
+# 4. Uzrakstiet programmu, kas izvada "15 un 85 summa ir" un pēc tam izvada šo skaitļu summu.
+print("15 un 85 summa ir", 15 + 85)
+
+# 5. Uzrakstiet programmu, kas izdrukā rezultātu, ienesot skaitļi 2 pakāpē 20.
+print(2**20)
+
+# 6. Uzrakstiet programmu, kas izdrukā 55 un 98 summu, kas ir reizināta ar 17.
+# PADOMS: varat izmantot iekavas (), lai norādītu secību, kādā tiek veikti matemātiskie aprēķini.
+print((55 + 98) * 17)
+
+# 7. Uzrakstiet programmu, kas 3 reizes pēc kārtas bez atstarpēm uz konsoles izdrukā vārdu "Apple".
+print("Apple" * 3)
+
+# 8. Uzrakstiet programmu, kas ekrānā parādīs virkni "Apple" tik reižu, cik skaitļā 15 ir skaitlis 2.
+print("Apple" * (15 // 2))
+
+# 9. Uzrakstiet programmu, kas ekrānā parādīs virkni “vvkvvkmvvkvvkm”. Bet jus varat izmantot atbildē tikai vienu reizi burtu "v", "k" un "m".
+# Tatad šī atbilde neder print("vvkvvkm" + "vvkvvkm"), jo taja atkartojas tie burti vairakas reizes!
+print((("v"*2 + "k")*2+"m")*2)
